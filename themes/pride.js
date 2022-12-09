183eb4358cd625b84322bbaca6825b042d7f8d0d
Collertabber <84198237+Collertabber@users.noreply.github.com>
2022-12-09 14:17:25 -0500

    Create pride.js

---
+++ themes/pride.js
@@ -0,0 +1,5 @@
+setCSSVar("--leftSidebarColor", "#262630");
+setCSSVar("--pageColor", "linear-gradient(to bottom, red, orange, yellow, green, blue, purple)");
+setCSSVar("--contentColor", "#EBEBEB"); setCSSVar("--contentColor2", "#E3E3E3");
+setCSSVar("--contentColor3", "#D9D9D9"); setCSSVar("--borderColor", "#323242");
+setCSSVar("--fontColor", "black"); setCSSVar("--themeColor", "tomato");
