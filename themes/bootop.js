54987db5657c654c3dc7a521d1457150e3d0cbf2
Collertabber <84198237+Collertabber@users.noreply.github.com>
2022-12-09 14:18:20 -0500

    Create bootop.js

---
+++ themes/bootop.js
@@ -0,0 +1,8 @@
+setCSSVar("--leftSidebarColor", "#262630");
+setCSSVar("--pageColor", "#151617");
+setCSSVar("--contentColor", "#1f1f28");
+setCSSVar("--contentColor2", "#24242e");
+setCSSVar("--contentColor3", "#2a2a37");
+setCSSVar("--borderColor", "#323242");
+setCSSVar("--fontColor", "#ffffff");
+setCSSVar("--themeColor", "#eb6123");
